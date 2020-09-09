147af434659d98992a620674cbcbd6fbb0a31b5b
althea <altheaserena.yi@gmail.com>
2020-09-09 16:10:10 -0400

    removes console logs

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -3,17 +3,14 @@ function loadMorePosts() {
   var _this = this;
   var $blogContainer = $(".blog-grid-container");
   var nextPage = parseInt($blogContainer.attr("data-page")) + 1;
-  console.log("NEXT PAGE", nextPage)
   var totalPages = parseInt($blogContainer.attr("data-totalPages"));
   $(this).addClass("loading");
 
 $.get("/jekyll-theme-memoirs/page" + nextPage, function (data) {
     var blogGridItems = $.parseHTML(data);
     var $articles = $(blogGridItems).find(".blog-grid-item");
-    console.log("ARTICLES", $articles)
-
     $blogContainer.attr("data-page", nextPage).append($articles);
-    console.log("CONTAINER", $blogContainer )
+
     if (totalPages == nextPage) {
         $(".loadMore").remove();
     }
