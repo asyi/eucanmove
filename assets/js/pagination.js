cbb7731034fb0a91bda680e6587e2a5487a97f58
althea <altheaserena.yi@gmail.com>
2020-09-10 15:40:19 -0400

    Adds pagination to tag pages

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,20 +1,49 @@
+"use strict";
+
 $(".loadMore").click(loadMorePosts);
+
 function loadMorePosts() {
   var _this = this;
+
+  var pathname = window.location.pathname;
+  console.log("path", pathname)
   var $blogContainer = $(".blog-grid-container");
-  var nextPage = parseInt($blogContainer.attr("data-page")) + 1;
-  var totalPages = parseInt($blogContainer.attr("data-totalPages"));
   $(this).addClass("loading");
 
-// if initial source is index
-$.get("/jekyll-theme-memoirs/page/" + nextPage, function (data) {
-    var blogGridItems = $.parseHTML(data);
-    var $articles = $(blogGridItems).find(".blog-grid-item");
-    $blogContainer.attr("data-page", nextPage).append($articles);
+  if (pathname.includes("/tags")) {
+    console.log("tag path")
+    var nextTag = parseInt($blogContainer.attr("data-tag")) + 1;
+    var totalTags = parseInt($blogContainer.attr("data-totalTags"));
 
-    if (totalPages == nextPage) {
+    $.get(pathname, function (data) {
+      var blogGridItems = $.parseHTML(data);
+      var $articles = $(blogGridItems).find(".blog-grid-item");
+      $blogContainer.attr("data-tag", nextTag).append($articles);
+      console.log("total tag", totalTags)
+      console.log("next tag", nextTag)
+
+      if (totalTags == nextTag) {
         $(".loadMore").remove();
-    }
-    $(_this).removeClass("loading");
+      }
+
+      $(_this).removeClass("loading");
     });
+  } else if (pathname.includes("/jekyll-theme-memoirs/")) {
+    console.log("main path")
+    var nextPage = parseInt($blogContainer.attr("data-page")) + 1;
+    var totalPages = parseInt($blogContainer.attr("data-totalPages"));
+    
+    $.get(pathname + "/page/" + nextPage, function (data) {
+      var blogGridItems = $.parseHTML(data);
+      var $articles = $(blogGridItems).find(".blog-grid-item");
+      $blogContainer.attr("data-page", nextPage).append($articles);
+      if (totalPages == nextPage) {
+        $(".loadMore").remove();
+      }
+    
+      $(_this).removeClass("loading");
+    });
+  }
 }
+
+
