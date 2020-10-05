6024b558dfe81f7b6a7b7dd8b0bdb3608bd4694e
althea <altheaserena.yi@gmail.com>
2020-10-05 00:31:45 -0400

    Removes extraneous console logs

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -17,17 +17,14 @@ function loadMorePosts() {
     let pathname = window.location.pathname;
     let hash = window.location.href
     let tag = hash.split('/').slice(-1).toString()
-    console.log("WINDOW HASHTAG", tag)
     let $blogContainer = $(".blog-grid-container");
     let $tagBlogContainer = $(`#blog-grid-container-${tag}`);
-    console.log("WINDOW HASH blog container", $tagBlogContainer)
     let $blogGridContainerSection5 = $(".blog-grid-container-section-5");
     let $blogGridContainerTagsFull = $(`#blog-grid-container-tags-full-${tag}`);
     $(this).addClass("loading");
 
     if (hash.includes("/tags") || hash.includes("/categories")) {
         let tagSectionTitle = $tagBlogContainer.attr("data-tagTitle");
-        console.log("Tag section title", tagSectionTitle)
         let tagSectionIndex = $tagBlogContainer.attr("data-tag");
         var totalTagPostsCount = parseInt($tagBlogContainer.attr("data-totalTagPostsCount"));
 
@@ -37,7 +34,6 @@ function loadMorePosts() {
             if (clickCount >= 2 && (remainder < 6)) {
                 // if there are less than 5 remaining posts, append them to the end
                 $.get(`${hash}#${tagSectionTitle}`, data => {
-                    console.log("HEREEEE")
                     let blogGridItems = $.parseHTML(data);
                     $tagPosts = $(blogGridItems).find(`#blog-grid-container-tags-hidden-${tagSectionTitle}`).find(".blog-grid-item").slice(sliceCount);
 
@@ -49,19 +45,12 @@ function loadMorePosts() {
                 });
             } else {
                 $.get(`${hash}#${tagSectionTitle}`, data => {
-                    console.log(`${hash}#${tagSectionTitle}`)
                     let blogGridItems = $.parseHTML(data);
                     $tagPosts = $(blogGridItems).find(`#blog-grid-container-tags-hidden-${tagSectionTitle}`).find(".blog-grid-item").slice(sliceCount);
-                    console.log("2-> TAG POSTS", $tagPosts)
-                    console.log("2-> Total tag post count", totalTagPostsCount)
                     tagPostsToLoad = totalTagPostsCount - 6
                     sliceCount = sliceCount + 6
                     remainder = tagPostsToLoad - sliceCount
                     loadPosts($tagPosts, 6, $blogGridContainerTagsFull, tagSectionIndex, totalTagPostsCount, _this);
-
-                    console.log("2-> Tag posts to load", tagPostsToLoad)
-                    console.log("2-> Slice count", sliceCount)
-                    console.log("2-> Remainder", remainder)
                 });
 
                 if (tagPostsToLoad === (sliceCount)) {
